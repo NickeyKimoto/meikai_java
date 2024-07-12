eec806ce2ef437ed2ced203acab8fdf802edfbf2
NickeyKimoto <ddt.nk.kimoto@gmail.com>
2024-07-12 17:17:32 +0900

    2024/07/12

--- MeikaiJava/List/Chap12/pet/Work.java
+++
@@ -1,5 +0,0 @@
-package MeikaiJava.List.Chap12.pet;
-
-public interface Work {
-	void work(int sw);
-}
--- MeikaiJava/List/Chap14/Wearable.java
+++
@@ -1,8 +0,0 @@
-package MeikaiJava.List.Chap14;
-
-// ウェアラブルインタフェース
-
-public interface Wearable {
-	void putOn(); // 着る
-	void putOff(); // 脱ぐ
-}
